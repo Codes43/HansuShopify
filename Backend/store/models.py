bc3a1f113fedf5c9f40d82adbe20e01cd0721a83
Albert Waneloba <waneroba22@gmail.com>
2025-05-28 13:22:25 +0300

    Successfully Created SignIn API to handle email and password

---
+++ Backend/store/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+class CustomUSer(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True) # Make sure this has unique=True
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    username=None
+
+    def __str__(self):
+        return self.email
---
+++ Backend/store/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model=CustomUSer
+        fields=[
+            'username',
+            'email',
+            'first_name',
+            'last_name'
+        ]
+            
